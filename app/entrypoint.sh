b6de042fdb87a52450a9617b5e88732f7a2be159
Chad Whitacre <chadwhitacre@sentry.io>
2021-11-09 17:18:50 -0500

    Working test

---
+++ app/entrypoint.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+read -p "Program: " program
+echo "Greetings, ${program}!"
--- install.sh
+++ install.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/env bash
 set -e
-docker compose down --remove-orphans testing
-docker compose build testing
-#exec &> >(tee -a "foo.log")
-docker compose --ansi never run --rm testing
+
+dcm1="docker-compose"
+dcm2="docker compose"
+args="--ansi never run --rm testing"
+
+# Setup
+$dcm2 down --remove-orphans testing
+$dcm2 build testing
+
+# Expected: prompt for 'Program:', accept input, print output.
+
+# Docker Compose v1 and v2 both work fine here.
+$dcm1 $args
+$dcm2 $args
+
+exec &> >(tee -a "log")
+
+# Docker Compose v1 works fine here ...
+$dcm1 $args
+
+# ... but Docker Compose v2 hangs.
+echo "Aaaaaaaaaaaaaaand ..."
+$dcm2 $args
